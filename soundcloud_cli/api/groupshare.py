0535f4bd4aa3166fc00bfb6c4cfdf54e6dea59a0
Norman Heckscher <norman.heckscher@gmail.com>
2016-01-25 18:15:26 +1000

    use string rather than decimal
    Signed-off-by: Norman Heckscher <norman.heckscher@gmail.com>

--- soundcloud_cli/api/groupshare.py
+++ soundcloud_cli/api/groupshare.py
@@ -7,4 +7,4 @@ def groupshare(group_id=None, track_id=None):
     client = get_client()
 
     # contribute track to group
-    return client.post('/groups/%d/contributions' % group_id, track={'id': track_id})
+    return client.post('/groups/%s/contributions' % group_id, track={'id': track_id})
